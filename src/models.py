841649bb8a204957f8306223a328983d33efe33f
Cat <126123826+CRob001@users.noreply.github.com>
2023-06-06 22:42:45 +0000

    Final Project

--- src/models.py
+++ src/models.py
@@ -7,23 +7,102 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def to_dict(self):
+        return {}
+
+
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=True)
+    mass = Column(String(250), nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    skin_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    gender = Column(String(250), nullable=True)
+    planets = Column(Integer, ForeignKey('planets.id'))
+    starships = Column(Integer, ForeignKey('starships.id'))
+    vehicles = Column(Integer, ForeignKey('vehicles.id'))
+
+    def to_dict(self):
+        return {}
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    rotational_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+
+    def to_dict(self):
+        return {}
+    
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmostphering_speed = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    MGLT = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+    pilots = Column(String(250), nullable=False)
+
+    def to_dict(self):
+        return {}
+    
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(256), nullable=True)
+    vehicle_class = Column(String(256), nullable=True)
+    manufacturer = Column(String(256), nullable=True)
+    length = Column(String(256), nullable=True)
+    cost_in_credits = Column(String(256), nullable=True)
+    crew = Column(String(256), nullable=True)
+    passengers = Column(String(256), nullable=True)
+    max_atmosphering_speed = Column(String(256), nullable=True)
+    cargo_capacity = Column(String(256), nullable=True)
+
+    def to_dict(self):
+        return {}
+    
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    fave_id = Column(Integer)
+    item_type = Column(String(256))
+    people = Column(Integer, ForeignKey('people.id'))
+    planets = Column(Integer, ForeignKey('planets.id'))
+    starships = Column(Integer, ForeignKey('starships.id'))
+    vehicles = Column(Integer, ForeignKey('vehicles.id'))
 
     def to_dict(self):
         return {}
